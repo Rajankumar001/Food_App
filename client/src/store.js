0bfc7a8adc98249650011b7d8f380cc64333d842
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-08-21 18:55:38 +0530

    changes tracked

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
 import {legacy_createStore,combineReducers,applyMiddleware} from 'redux'
-import {thunk} from 'redux-thunk';
+import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {getFoodReducers,addFoodReducers,editFoodReducers,} from './reducers/FoodReducers';
 import { CartReducer } from './reducers/CartReducers';
